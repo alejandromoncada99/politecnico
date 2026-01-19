1ed908c045086fdab9e6cf96e50a6024ae0bdae0
alejandromoncada99 <alejandromoncadacarder2025@gmail.com>
2026-01-19 15:49:44 -0500

    Restrict ALLOWED_HOSTS to .onrender.com

--- configuracion/settings.py
+++ configuracion/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fi_-nq$#=9y%*z_hbzudtdh5%o&vro)#8fib%ud(zlg!-f_wnt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".onrender.com"]
 
 
 
